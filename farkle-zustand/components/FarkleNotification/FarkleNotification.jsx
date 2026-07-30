eb110cb974c7228ca336e222d9baa98467165907
Peter Marsh <peter_aaron_marsh@hotmail.com>
2026-07-30 18:26:48 +0900

    adjusted notification delay

--- farkle-zustand/components/FarkleNotification/FarkleNotification.jsx
+++ farkle-zustand/components/FarkleNotification/FarkleNotification.jsx
@@ -8,9 +8,22 @@ export default function FarkleNotification({ status }) {
         {status === 'farkle' && (
           <motion.div
             initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.3 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                duration: 0.3,
+                delay: 0.5,
+              },
+            }}
+            exit={{
+              opacity: 0,
+              y: -20,
+              transition: {
+                duration: 0.3,
+                delay: 0,
+              },
+            }}
             style={{
               position: "absolute",
               width: '100%'
