0f85f7c4111e5f1baa30e24564eed14ed3f8c421
Peter Marsh <peter_aaron_marsh@hotmail.com>
2026-07-31 15:38:43 +0900

    added start of layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FarkleGamePage from "./pages/FarkleGamePage"
 import { useEffect } from 'react'
 import { useUserActions } from "./stores/user"
 import farkleServcie from "./services/farkle"
+import Layout from "./components/Layout/Layout"
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   const { setUser } = useUserActions()
@@ -19,13 +21,15 @@ function App() {
   }, [])
 
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='/farkle' element={<FarkleGamePage/>} />
-    </Routes>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='farkle' element={<FarkleGamePage />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
